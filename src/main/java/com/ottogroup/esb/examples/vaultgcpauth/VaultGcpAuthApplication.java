da6d1091a25784c6560e5324056c2e125a772292
Daniel Dethloff <Daniel.Dethloff@otto.de>
2023-01-26 10:08:29 +0100

    MWTEAM-0000 remove url

--- src/main/java/com/ottogroup/esb/examples/vaultgcpauth/VaultGcpAuthApplication.java
+++ src/main/java/com/ottogroup/esb/examples/vaultgcpauth/VaultGcpAuthApplication.java
@@ -6,14 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class VaultGcpAuthApplication {
-	@RequestMapping("/")
-	public String home() {
-		return "Hello World!";
-	}
 	public static void main(String[] args) {
 		SpringApplication.run(VaultGcpAuthApplication.class, args);
 	}
-
-
-
 }
